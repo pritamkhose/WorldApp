fb3819e19aa95bc9f58292a1678ed076b8e7fc2c
Pritam Khose <pritamkhose@users.noreply.github.com>
2017-11-18 07:46:54 +0530

    v2
    v2

---
+++ src/java/com/pritam/DAO/AppConstant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pritam.DAO;
+
+/**
+ *
+ * @author Pritam
+ */
+public class AppConstant {
+
+    public static String jdbcURL = "jdbc:mysql://localhost:3306/world";
+    public static String jdbcClass = "com.mysql.jdbc.Driver";
+    public static String jdbcUsername = "root";
+    public static String jdbcPassword = "root";
+
+}
